330c4216597e8a62abc5eaf62a14441adb9a5ce2
Akshaya Dash <akshaya.dash@kare4u.in>
2018-10-16 19:31:38 +0530

    Initial with asp.net identity mysql

---
+++ CodeBase/Server/HireMe/HireMe/Models/MySqlIdentityConfiguration/MySqlConfiguration.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Migrations.History;
+using System.Linq;
+using System.Web;
+
+namespace HireMe.Models.MySqlIdentityConfiguration
+{
+    public class MySqlConfiguration : DbConfiguration
+    {
+        public MySqlConfiguration()
+        {
+            SetHistoryContext(
+            "MySql.Data.MySqlClient", (conn, schema) => new MySqlHistoryContext(conn, schema));
+        }
+    }
+
+    public class MySqlHistoryContext : HistoryContext
+    {
+        public MySqlHistoryContext(
+          DbConnection existingConnection,
+          string defaultSchema)
+        : base(existingConnection, defaultSchema)
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<HistoryRow>().Property(h => h.MigrationId).HasMaxLength(100).IsRequired();
+            modelBuilder.Entity<HistoryRow>().Property(h => h.ContextKey).HasMaxLength(200).IsRequired();
+        }
+    }
+
+    public class MySqlInitializer : IDatabaseInitializer<ApplicationDbContext>
+    {
+        public void InitializeDatabase(ApplicationDbContext context)
+        {
+            if (!context.Database.Exists())
+            {
+                // if database did not exist before - create it
+                context.Database.Create();
+            }
+            else
+            {
+                // query to check if MigrationHistory table is present in the database 
+                var migrationHistoryTableExists = ((IObjectContextAdapter)context).ObjectContext.ExecuteStoreQuery<int>(
+                "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = 'hireme' AND table_name = '__MigrationHistory'");
+
+                // if MigrationHistory table is not there (which is the case first time we run) - create it
+                if (migrationHistoryTableExists.FirstOrDefault() == 0)
+                {
+                    context.Database.Delete();
+                    context.Database.Create();
+                }
+            }
+        }
+    }
+}
