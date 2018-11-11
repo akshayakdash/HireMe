c058727e2a693d5f8f18c5961101d74597543802
Bhairab Charan Meher <bhairabonweb@gmail.com>
2018-11-11 15:56:31 +0530

    New Page Changes

---
+++ CodeBase/Server/HireMe/HireMe/Controllers/DemoCandidateController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HireMe.Models;
+
+namespace HireMe.Controllers
+{
+    public class DemoCandidateController : Controller
+    {
+      
+        public ActionResult DashBoard()
+        {
+            return View();
+        }
+        public ActionResult SearchJob()
+        {
+            return View();
+        }
+        public ActionResult ProfileCardOption()
+        {
+            return View();
+        }
+
+        public ActionResult PublishJob()
+        {
+            return View();
+        }
+        public ActionResult SC1()
+        {
+            return View();
+        }
+        public ActionResult SC2()
+        {
+            return View();
+        }
+        public ActionResult ManageFavo()
+        {
+            return View();
+        }
+    }
+}
