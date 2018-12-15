8b7a088c674927d08a84a9eeaf200a41353ad1f3
susant <susant.patra.77@gmail.com>
2018-12-15 22:16:39 +0530

    bug fixed

--- CodeBase/Server/HireMe/HireMe/Controllers/HomeController.cs
+++ CodeBase/Server/HireMe/HireMe/Controllers/HomeController.cs
@@ -41,31 +41,31 @@ namespace HireMe.Controllers
 
             // In Startup iam creating first Admin Role and creating a default Admin User     
 
-            if (!roleManager.RoleExists("Admin"))
-            {
+            //if (!roleManager.RoleExists("Admin"))
+            //{
 
-                // first we create Admin rool    
-                //var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
-                //role.Name = "Admin";
-                //roleManager.Create(role);
+            //    // first we create Admin rool    
+            //    //var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+            //    //role.Name = "Admin";
+            //    //roleManager.Create(role);
 
-                //Here we create a Admin super user who will maintain the website                   
+            //    //Here we create a Admin super user who will maintain the website                   
 
-                var user = new ApplicationUser();
-                user.UserName = "Admin";
-                user.Email = "admin1@gmail.com";
+            //    var user = new ApplicationUser();
+            //    user.UserName = "Admin";
+            //    user.Email = "admin1@gmail.com";
 
-                string userPWD = "Akshaya@123";
+            //    string userPWD = "Akshaya@123";
 
-                var chkUser = UserManager.Create(user, userPWD);
+            //    var chkUser = UserManager.Create(user, userPWD);
 
-                //Add default User to Role Admin    
-                if (chkUser.Succeeded)
-                {
-                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+            //    //Add default User to Role Admin    
+            //    if (chkUser.Succeeded)
+            //    {
+            //        var result1 = UserManager.AddToRole(user.Id, "Admin");
 
-                }
-            }
+            //    }
+            //}
 
             // creating Creating Manager role     
             //if (!roleManager.RoleExists("Employer"))
