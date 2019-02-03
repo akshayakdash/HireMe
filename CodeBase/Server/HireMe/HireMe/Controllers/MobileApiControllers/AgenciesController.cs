f0383d6f4b9b06b53b70b9dbd53bfe6fc112770a
Akshaya Dash <akshaya.dash@kare4u.in>
2019-02-03 15:15:52 +0530

    mobile api controller for job request and job offer added....

--- CodeBase/Server/HireMe/HireMe/Controllers/MobileApiControllers/AgenciesController.cs
+++ CodeBase/Server/HireMe/HireMe/Controllers/MobileApiControllers/AgenciesController.cs
@@ -1,13 +1,90 @@
-﻿using System;
+﻿using HireMe.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Formatting;
 using System.Web.Http;
+using System.Data.Entity;
+using HireMe.Utility;
 
 namespace HireMe.Controllers.MobileApiControllers
 {
     public class AgenciesController : ApiController
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        private JsonMediaTypeFormatter jsonFormatter = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+
+        public AgenciesController()
+        {
+            var jSettings = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+                DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+            };
+
+            jsonFormatter.SerializerSettings = jSettings;
+        }
+
+        [HttpGet]
+        [Route("api/Agencies/{agencyId}/Candidates")]
+        public HttpResponseMessage MyCandidates(int agencyId)
+        {
+            var candidates = db.Candidates.Include(p => p.ApplicationUser).Where(p => p.AgencyId == agencyId && p.StaffType == StaffType.Agency).ToList();
+            return Request.CreateResponse(HttpStatusCode.OK, candidates, jsonFormatter);
+        }
+
+        [HttpGet]
+        [Route("api/Agencies/{agencyId}/Candidates/{candidateId}/Activate")]
+        public HttpResponseMessage ActivateCandidate(int agencyId, int candidateId)
+        {
+            var agency = db.Agencies.Find(agencyId);
+            if (agency == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, "Agency Not Found");
+            }
+
+            var candidate = db.Candidates.Find(candidateId);
+            // if candidate is null throw an exception
+            if (candidate == null)
+                return Request.CreateResponse(HttpStatusCode.NotFound, "Candidate Not Found.");
+            candidate.ProfileVerified = true;
+            db.SaveChanges();
+
+            NotificationFramework.SendNotification(agency.AspNetUserId, candidate.AspNetUserId, "Candidate Account Activation - JOBTek", "Your candidate Account " + candidate.FirstName + " was activated by Agency" + agency.AgencyName + " on " + DateTime.Now.Date.ToString("dd-MMM-yyyy"), 0, true);
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+
+        [HttpGet]
+        [Route("api/Agencies/{agencyId}/JobRequests")]
+        public HttpResponseMessage MyJobRequests(int agencyId)
+        {
+          
+            var agency = db.Agencies.Find(agencyId);
+            if (agency != null)
+            {
+                var jobRequests = db.JobRequests.Include(path => path.Job).Include(t => t.Candidate).ToList();
+                jobRequests = jobRequests.Where(p => p.Candidate.StaffType == StaffType.Agency && p.Candidate.AgencyId == agency.AgencyId).ToList();
+                return Request.CreateResponse(HttpStatusCode.OK, jobRequests, jsonFormatter);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
 }
---
+++ CodeBase/Server/HireMe/HireMe/Controllers/MobileApiControllers/JobOffersController.cs
@@ -0,0 +1,95 @@
+﻿using HireMe.Models;
+using HireMe.Utility;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using System.Data.Entity;
+using System.Linq.Dynamic;
+
+namespace HireMe.Controllers.MobileApiControllers
+{
+    public class JobOffersController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        private JsonMediaTypeFormatter jsonFormatter = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+
+        public JobOffersController()
+        {
+            var jSettings = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+                DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+            };
+
+            jsonFormatter.SerializerSettings = jSettings;
+        }
+
+        [HttpGet]
+        [Route("api/JobOffers")]
+        public HttpResponseMessage SearchJobOffers([FromUri]JobOfferSearchParam searchParam = null)
+        {
+            if (searchParam != null)
+            {
+                object[] queryString = searchParam.GetSearchQuery();
+                ArrayList searchArgs = (ArrayList)queryString[1];
+                var jobOffers = db.JobOffers
+                    .Include(j => j.Employer)
+                    .Include(j => j.Job)
+                    .Include(j => j.JobOfferJobTasks)
+                    .AsQueryable()
+                    .Where(queryString[0].ToString(), searchArgs.ToArray()).ToList();
+
+                if (searchParam != null && searchParam.Tasks != null && searchParam.Tasks.Count > 0)
+                {
+                    jobOffers = jobOffers
+                        .Where(p => p.JobOfferJobTasks.Select(t => t.JobTaskId).Any(c => searchParam.Tasks.Contains(c))).ToList();
+                }
+                return Request.CreateResponse(HttpStatusCode.OK, jobOffers.OrderByDescending(p => p.JobOfferId).ToList(), jsonFormatter);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, db.JobOffers
+                    .Include(j => j.Employer)
+                    .Include(j => j.Job)
+                    .Include(j => j.JobOfferJobTasks)
+                    .OrderByDescending(p => p.JobOfferId).ToList(), jsonFormatter);
+            }
+        }
+
+        [HttpGet]
+        [Route("api/JobOffers/{jobOfferId}")]
+        public HttpResponseMessage JobOfferDetails(int jobOfferId)
+        {
+
+            JobOffer jobOffer = db.JobOffers
+                .Include(t => t.JobOfferJobTasks)
+                .Include(p => p.Employer)
+                .Include(t => t.Job)
+                .FirstOrDefault(c => c.JobOfferId == jobOfferId);
+            if (jobOffer == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            jobOffer.MasterJobTasks = db.Jobs
+                .Include(p => p.JobTasks)
+                .FirstOrDefault(p => p.JobId == jobOffer.JobId)
+                .JobTasks;
+
+            //var userId = jobOffer.Employer.AspNetUserId;
+            //// get all the feedbacks given to the user
+            //var userFeedbacks = db.UserFeedbacks.Include(p => p.Sender).Where(p => p.ReceiverId == userId);
+            //ViewBag.UserFeedbacks = userFeedbacks.ToList();
+
+
+            return Request.CreateResponse(HttpStatusCode.OK, jobOffer, jsonFormatter);
+        }
+    }
+}
